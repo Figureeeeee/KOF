1e59a66194802da0a3e3b9eb76d4ae824330b6ef
Figureeeeee <1157552798@qq.com>
2022-11-09 20:32:20 +0800

    start project

---
+++ README.md
@@ -0,0 +1,19 @@
+# KOF
+
+## 总览
+目标：实现一个拳皇demo
+技术栈：HTML5、CSS3、JavaScript、Git
+ps:JavaScript的一些常用库：jQuery、requestAnimationFrame、canvas
+
+## 按日期写总结
+
+### 2022/11/9：
+主要成果：
+* 1.实现一个游戏主页面，包含地图以及模拟人物的两个矩形
+* 2.通过封装、继承实现了父类`AcGameObject`的子类`GameMap`、`Player`
+* 3.利用`requestAnimationFrame`实现父类`AcGameObject`的`update()`函数（以下称为帧函数），实现按帧执行动作，以实现流畅的动画（有点像u3d的生命周期函数hhh）
+* 4.在`Player`对象中通过每一帧的时间模拟速度、加速度、位移，并通过帧函数模拟一个状态机（默认情况下为idle状态，根据键鼠输入改变状态，后续可扩展加入触屏按压模式）
+
+
+
+待后续完善...
---
+++ static/js/player/base.js
@@ -0,0 +1,104 @@
+import { AcGameObject } from "/static/js/ac_game_object/base.js";
+
+export class Player extends AcGameObject {
+    constructor(root, info) {
+        super();
+
+        this.root = root;
+
+        this.id = info.id;
+        this.x = info.x;
+        this.y = info.y;
+        this.width = info.width;
+        this.height = info.height;
+        this.color = info.color;
+
+        this.direction = 1;
+
+        this.vx = 0;
+        this.vy = 0;
+
+        this.speedx = 400;  // 水平速度
+        this.speedy = -1000;  // 跳起的初始速度
+
+        this.gravity = 50;
+
+        this.ctx = this.root.game_map.ctx;
+        this.pressed_keys = this.root.game_map.controller.pressed_keys;
+
+        // 0:idle, 1:forward, 2:backward, 3:jump, 4:attack, 5:beattacked, 6:dead
+        this.status = 3;
+    }
+
+    start() {
+
+    }
+
+    update_control() {
+        let w, a, d, space;
+        if (this.id === 0) {
+            w = this.pressed_keys.has('w');
+            a = this.pressed_keys.has('a');
+            d = this.pressed_keys.has('d');
+            space = this.pressed_keys.has(' ');
+        } else {
+            w = this.pressed_keys.has('ArrowUp');
+            a = this.pressed_keys.has('ArrowLeft');
+            d = this.pressed_keys.has('ArrowRight');
+            space = this.pressed_keys.has('Enter');
+        }
+
+        if (this.status === 0 || this.status === 1) {
+            if (w) {
+                if (d) {
+                    this.vx = this.speedx;
+                } else if (a) {
+                    this.vx = -this.speedx;
+                } else {
+                    this.vx = 0;
+                }
+                this.vy = this.speedy;
+                this.status = 3;
+            } else if (d) {
+                this.vx = this.speedx;
+                this.status = 1;
+            } else if (a) {
+                this.vx = -this.speedx;
+                this.status = 1;
+            } else {
+                this.vx = 0;
+                this.status = 0;
+            }
+        }
+    }
+
+    update_move() {
+        this.vy += this.gravity;
+        this.x += this.vx * this.timedelta / 1000;
+        this.y += this.vy * this.timedelta / 1000;
+
+        if (this.y > 450) {
+            this.y = 450;
+            this.vy = 0;
+            this.status = 0;
+        }
+
+        if (this.x < 0) {
+            this.x = 0;
+        } else if (this.x + this.width > this.root.game_map.$canvas.width()) {
+            this.x = this.root.game_map.$canvas.width() - this.width;
+        }
+    }
+
+    update() {
+        this.update_control();
+        this.update_move();
+
+        this.render();
+    }
+
+    render() {
+        this.ctx.fillStyle = this.color;
+        this.ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
